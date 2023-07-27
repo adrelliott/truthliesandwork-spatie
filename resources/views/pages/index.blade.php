56f2938b9bdf36406a46ce12c9b20f83e054272e
Al Elliott <adrelliott@gmail.com>
2023-07-27 17:09:32 +0200

    Stub out layout and sample index view

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,3 +1,7 @@
 <x-layouts.public>
-    This is the index.php
+    <x-slot name="header">
+        <h1 class="text-red-400 text-3xl">
+            This is the index.php
+        </h1>
+    </x-slot name="header">
 </x-layouts.public>
