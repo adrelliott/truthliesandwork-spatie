5b26d0a5f8f2bc5837b6c67d57373bc920ff52d5
Al Elliott <adrelliott@gmail.com>
2023-07-27 21:37:31 +0200

    Implementing the episode pages

---
+++ resources/views/pages/episodes/[Episode:slug].blade.php
@@ -0,0 +1,9 @@
+<x-public>
+    Episode Title: {{ $episode->title }}
+    <br>
+    Episode Description: {{ $episode->description }}
+    <br>
+    Episode Slug: {{ $episode->slug }}
+    <br>
+    <a href="/episodes">&leftarrow; Back to all episodes</a>
+</x-public>
---
+++ resources/views/pages/episodes/index.blade.php
@@ -0,0 +1,11 @@
+@php
+    $episodes = App\Models\Episode::all();
+@endphp
+<x-public>
+    This would list all the episodes
+    @foreach($episodes as $episode)
+        <div>
+            <a href="/episodes/{{ $episode->slug }}">{{ $episode->title }}</a>
+        </div>
+    @endforeach
+</x-public>
