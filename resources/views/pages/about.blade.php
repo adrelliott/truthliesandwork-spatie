72d070fd9c82bdd59934b058863e2a6bf829b809
Al Elliott <adrelliott@gmail.com>
2023-07-27 21:37:09 +0200

    Creating some page stubs

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+<x-public>
+    <x-layout.section-wide>
+        <x-elements.image-left>
+            <x-slot name="image">
+                <img src="https://via.placeholder.com/500x500" alt="">
+            </x-slot>
+            <h1>Image left</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </x-elements.image-left>
+    </x-layout.section-wide>
+</x-public>
---
+++ resources/views/pages/lp/rx7.blade.php
@@ -0,0 +1,11 @@
+<x-public hideNav="true" hideFooter="true" pageHeader="Ladning page">
+    <x-layout.section-wide>
+        <x-elements.image-left>
+            <x-slot name="image">
+                <img src="https://via.placeholder.com/500x500" alt="">
+            </x-slot>
+            <h1>Image left</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </x-elements.image-left>
+    </x-layout.section-wide>
+</x-public>
